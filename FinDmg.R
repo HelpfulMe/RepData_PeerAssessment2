9cc8b868e2923e30dace3dcac258c4c588c19c35
HelpfulMe <beccasmithusa@gmail.com>
2016-10-15 00:31:18 -0400

    Updated FinDmg.R

--- FinDmg.R
+++ FinDmg.R
@@ -4,5 +4,5 @@ fin <- aggregate(df$TOTDMGDLRS, by=list(Category=df$EVT), FUN=sum)
 
 fin <- fin[order(-fin$x),]
 
-p <- ggplot(data=fin, aes(x=fin$x, y=fin$Category) + geom_point(size=3))
-p
+fin15<- fin[1:15,]
+plot(hist(fin$x))
